5ea0778ebfb15005d9436154322a90fa014bbbe8
Alex Smith <alex.smith1815@gmail.com>
2021-06-22 21:26:53 +0000

    cleaned up file for readability

--- chessbot.py
+++ chessbot.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3.8
+# Main file to run for function with lichess.org
+# Most code is borrowed from berserk documentation
+# and adjusted for use in this project
 import berserk
 import threading
 import chess
 import testgame
 
+# Game class used for accepting challenges and streaming ongoing games
 class Game(threading.Thread):
     def __init__(self, client, game_id, **kwargs):
         super().__init__(**kwargs)
@@ -13,6 +17,7 @@ class Game(threading.Thread):
         self.current_state = next(self.stream)
         self.mess = 1
 
+    # Responds to move with one of its own or responds to chat message
     def run(self):
         for event in self.stream:
             if event['type'] == 'gameState':
@@ -20,37 +25,44 @@ class Game(threading.Thread):
             elif event['type'] == 'chatLine':
                 self.handle_chat_line(event)
 
+    # Makes a move if the game state has changed
     def handle_state_change(self, game_state):
-        b = chess.Board()
+        # Creates the board locally from list of moves played in the game
+        board = chess.Board()
         moves = game_state['moves']
         move_list = moves.split()
         for m in move_list:
-            b.push(chess.Move.from_uci(m))
-        if b.turn == chess.BLACK:
+            board.push(chess.Move.from_uci(m))
+            
+        if board.turn == chess.BLACK:
             print("Generating move...")
-            client.bots.make_move(self.game_id, testgame.getMove(b))
+            client.bots.make_move(self.game_id, testgame.getMove(board))
 
+    # Says one simple message in response to chat
     def handle_chat_line(self, chat_line):
         if self.mess == 1:
             client.bots.post_message(self.game_id, 'Prepare to lose')
             self.mess = 0
 
+# Function to be adjusted if type of game wants to be evaluated before accepting
 def should_accept(self, chal=""):
     return True
 
 if __name__ == "__main__":
+    # Key for bot API (please don't abuse...)
     session = berserk.TokenSession("t7b1Sb9Zp788Fp5b")
     client = berserk.Client(session=session)
 
+    # Used to show that the program has successfully connected to bot with API
     print(client.account.get_email())
 
-    is_polite = True
+    # Loops through incoming events the bot recieves
     for event in client.bots.stream_incoming_events():
+        # Accepts any challenges
         if event['type'] == 'challenge':
             if should_accept(event):
                 client.bots.accept_challenge(event['challenge']['id'])
-            elif is_polite:
-                client.bots.decline_challenge(event['id'])
+        # If a game has started we launch a Game object to play the moves
         elif event['type'] == 'gameStart':
             game = Game(client, event['game']['id'])
             game.run()
